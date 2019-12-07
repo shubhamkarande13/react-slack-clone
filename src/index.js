9950a1b75c64f4843fe33890597ebce45b775b2b
Shubham Karande <shubhamkarade13@gmail.com>
2019-12-07 19:36:39 +0530

    add auth

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => (
+    <Router>
+        <Switch>
+            <Route path="/" component={App} />
+        </Switch>
+    </Router>
+);
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
