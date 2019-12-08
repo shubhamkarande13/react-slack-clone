64512d3130ecfa9b793d870e2d8c9dd857e4bf40
Shubham Karande <shubhamkarade13@gmail.com>
2019-12-08 18:05:36 +0530

    add authentication

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Grid,Form ,Segment ,Button,Header,Message,Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom/Link';
+class Register extends React.Component {
+state = {} 
+handleChange = () => {}
+render(){
+    return(
+        <Grid textAlign="center" verticalAlign="middle" className="app">
+            <Grid.Column style={{ maxWidth:450}}>
+                <Header as="h2" icon color="orange" textAlign="center">
+                    <Icon name="code" color="orange"/>    
+                    Register for DevChat
+                </Header>
+                <Form size="large">
+                    <Segment stacked>
+                        <Form.Input fluid name="username" icon="user" iconPosition="left" 
+                            placeholder="Username" onChange={this.handleChange} type="email"/>
+                        <Form.Input fluid name="password" icon="lock" iconPosition="left" 
+                            placeholder="Password" onChange={this.handleChange} type="password"/>
+                        <Form.Input fluid name="password" icon="repeat" iconPosition="left" 
+                            placeholder="Password Confirmation" onChange={this.handleChange} type="password"/>
+                        <Button color="orange" fluid size ="large">Submit</Button>                                               
+                    </Segment>
+                </Form>   
+                <Message>Already a User?<Link to="/login"></Link></Message>
+            </Grid.Column>
+        </Grid>
+        );
+    }
+}
+export default Register;
