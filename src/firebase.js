e3f5cb118995b1057699bfb7431a369e8b5033e5
Shubham Karande <shubhamkarade13@gmail.com>
2019-12-08 18:24:47 +0530

    add auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,16 @@
-import firebase from firebase/app;
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBNlqK4HR6NlPKYbSPQ3pCn3yF5-Zy0oQs",
-    authDomain: "react-slack-clone-e0d57.firebaseapp.com",
-    databaseURL: "https://react-slack-clone-e0d57.firebaseio.com",
-    projectId: "react-slack-clone-e0d57",
-    storageBucket: "react-slack-clone-e0d57.appspot.com",
-    messagingSenderId: "698306519945",
-    appId: "1:698306519945:web:eda609a164295dd6bab758",
-    measurementId: "G-Z5HTS35Z76"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  export default firebase;
+var config = {
+  apiKey: "AIzaSyCREQMYIbAYKscrS_JYvWP4KpMsF_jI5Bg",
+  authDomain: "react-slack-clone.firebaseapp.com",
+  databaseURL: "https://react-slack-clone.firebaseio.com",
+  projectId: "react-slack-clone",
+  storageBucket: "react-slack-clone.appspot.com",
+  messagingSenderId: "423395756405"
+};
+firebase.initializeApp(config);
+
+export default firebase;
